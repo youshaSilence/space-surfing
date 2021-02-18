0a5d9e2fdbdfb1d0f64efbb5d77a1847d79964a2
Ksiusha <mksyousha2014@gmail.com>
2021-02-18 07:21:56 +0300

    Движется, но по-дурацки

--- src/js/spaceship.js
+++ src/js/spaceship.js
@@ -1,5 +1,9 @@
 import * as PIXI from 'pixi.js';
+import * as Matter from 'matter-js';
 import keyboard from './keyboard';
+import { getXPos, getYPos } from './utils';
+
+const { Bodies, Body } = Matter;
 
 export default class Spaceship extends PIXI.Sprite {
   constructor({
@@ -13,65 +17,108 @@ export default class Spaceship extends PIXI.Sprite {
     this.anchor.set(0.5, 0.5);
     this.position.set(x, y);
 
-    this.vx = 0;
-    this.vy = 0;
+    this.maxSpeed = 5;
+    this.currentSpeed = 0;
+    this.acceleration = 0.07;
+
+    this.maxAngularSpeed = 0.05;
+    this.currentAngularSpeed = 0;
+    this.angularAcceleration = 0.002;
+
+    this.body = Bodies.polygon(
+      x, y, 3, width / 2, { restitution: 0.2 },
+    );
+
+    // Body.setInertia(this.body, 100);
+    //Body.setMass(this.body, 1000);
+    // this.body.friction = 0.1;
 
     this.setControls();
   }
 
   setControls() {
-    const left = keyboard('ArrowLeft');
-    const right = keyboard('ArrowRight');
-    const up = keyboard('ArrowUp');
-    const down = keyboard('ArrowDown');
+    this.keyboard = {};
+    this.keyboard.left = keyboard('ArrowLeft');
+    this.keyboard.right = keyboard('ArrowRight');
+    this.keyboard.up = keyboard('ArrowUp');
+    this.keyboard.down = keyboard('ArrowDown');
     // const space = keyboard(' ');
 
-    left.press = () => {
-      this.vy = 0;
-      this.vx = -5;
+    this.keyboard.left.press = () => {
+      // this.body.torque = -0.1;
+      // this.vr = -0.03;
     };
 
-    left.release = () => {
-      if (!right.isDown && this.vy === 0) {
-        this.vx = 0;
-      }
+    this.keyboard.left.release = () => {
+      this.currentAngularSpeed = 0;
+      // this.vr = 0;
     };
 
-    up.press = () => {
-      this.vy = -5;
-      this.vx = 0;
+    this.keyboard.up.press = () => {
+      this.currentSpeed = this.body.speed;
+      // this.vspeed = 1;
     };
-    up.release = () => {
-      if (!down.isDown && this.vx === 0) {
-        this.vy = 0;
-      }
-    };
-
-    // Right
-    right.press = () => {
-      this.vx = 5;
-      this.vy = 0;
-    };
-    right.release = () => {
-      if (!left.isDown && this.vy === 0) {
-        this.vx = 0;
-      }
+    this.keyboard.up.release = () => {
+      // this.currentSpeed = 0;
+      // this.vspeed = 0;
     };
 
-    // Down
-    down.press = () => {
-      this.vy = 5;
-      this.vx = 0;
+    this.keyboard.right.press = () => {
+      // this.vr = 0.03;
     };
-    down.release = () => {
-      if (!up.isDown && this.vx === 0) {
-        this.vy = 0;
-      }
+    this.keyboard.right.release = () => {
+      this.currentAngularSpeed = 0;
+      // this.vr = 0;
+    };
+
+    this.keyboard.down.press = () => {
+      // this.currentSpeed = this.body.speed;
+      // this.currentSpeed -= 0.5;
+    };
+
+    this.keyboard.down.release = () => {
     };
   }
 
-  update() {
-    this.y += this.vy;
-    this.x += this.vx;
+  update(delta, { bodyPosition, bodyAngle }) {
+    if (this.keyboard.up.isDown) {
+      // if (this.currentSpeed < this.maxSpeed) {
+      //   this.currentSpeed += this.acceleration;
+      // }
+
+      const y = getYPos(this.body.angle, this.body.speed + this.acceleration);
+      const x = getXPos(this.body.angle, this.body.speed + this.acceleration);
+      Body.setVelocity(this.body, { x, y: -y });
+    }
+
+    if (this.keyboard.down.isDown) {
+      const y = getYPos(this.body.angle, this.body.speed - 0.01);
+      const x = getXPos(this.body.angle, this.body.speed - 0.01);
+
+      Body.setVelocity(this.body, { x, y: -y });
+    }
+
+    if (this.keyboard.left.isDown) {
+      if (this.currentAngularSpeed < this.maxAngularSpeed) {
+        this.currentAngularSpeed += this.angularAcceleration;
+      }
+
+      Body.setAngularVelocity(this.body, -this.currentAngularSpeed);
+    }
+
+    if (this.keyboard.right.isDown) {
+      if (this.currentAngularSpeed < this.maxAngularSpeed) {
+        this.currentAngularSpeed += this.angularAcceleration;
+      }
+
+      Body.setAngularVelocity(this.body, this.currentAngularSpeed);
+    }
+
+    // this.body.torque = this.vr;
+
+    this.position = bodyPosition;
+    this.rotation = bodyAngle;
+    // this.y += this.vy;
+    // this.x += this.vx;
   }
 }
---
+++ src/js/spaceshipBody.js
@@ -0,0 +1,7 @@
+import * as Matter from 'matter-js';
+
+const { Bodies } = Matter;
+
+export default class SpaceshipBody extends Bodies {
+  
+}
